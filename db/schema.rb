cd717744699f697c6aee8e4b4ee0f634c96fc925
Kurt Chi <wtfisgn@gmail.com>
2015-09-13 18:15:25 +0800

    建立一個討論版(group)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901153411) do
+ActiveRecord::Schema.define(version: 20150913100924) do
+
+  create_table "groups", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "topics", force: true do |t|
     t.string   "title"
