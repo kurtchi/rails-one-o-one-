6af3dc8ea4a6813df705d441446195e810a9b354
Kurt Chi <wtfisgn@gmail.com>
2015-10-29 22:16:04 +0800

    [5] 建立 New action/Create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,34 @@
 class PostsController < ApplicationController
 
 	def new
+	   @group = Group.find(params[:group_id])
+       @post = @group.posts.new
 	end
 	
 	def edit
 	end
 	
 	def create
+		@group = Group.find(params[:group_id])
+		@post = @group.posts.build(post_params)
+
+		if @post.save
+			redirect_to group_path(@group), notice: "新增文章成功！"
+		else
+			render :new
+		end
 	end
 	
 	def update
 	end
 	
 	def destroy
+	end
+
+
+	private
+	def post_params
+		params.require(:post).permit(:content)
 	end
 
 end
