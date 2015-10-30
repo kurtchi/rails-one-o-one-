b6fb021efa9326223a1038d6fc273dd576900625
Kurt Chi <wtfisgn@gmail.com>
2015-10-30 23:56:17 +0800

    [6] 客製化欄位：name/客製化 devise 的 views => 註冊頁面新增 name 欄位

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029144941) do
+ActiveRecord::Schema.define(version: 20151030154959) do
 
   create_table "groups", force: true do |t|
     t.string   "title"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20151029144941) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
